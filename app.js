7bae43a9d8229cb0173cb8a0704e440a887c760c
Efornies <eleforbel@gmail.com>
2022-03-26 10:57:39 +0000

    final

--- app.js
+++ app.js
@@ -10,10 +10,29 @@ console.log(sum(7,3))
 // (similar to the keyword `export` when using webpack)
 module.exports = { sum };
 
-// one euro is:
-let oneEuroIs = {
-    "JPY": 127.9, // japan yen
-    "USD": 1.2, // us dollar
-    "GBP": 0.8, // british pound
+// this is my function that convert from dollar/euro/yann to each currency
+
+
+let oneEuroIs  = {
+    "JPY": 127.9, 
+    "USD": 1.2, 
+    "GBP": 0.8, 
 }
 
+const fromDollarToYen = (dollar) => {
+    return (dollar / oneEuroIs['USD']) * oneEuroIs['JPY']
+}
+
+const fromEuroToDollar = (euro) => {
+    return euro * oneEuroIs['USD']
+}
+
+const fromYanToPound = (yen) => {
+    return (yen / oneEuroIs['JPY']) * oneEuroIs['GBP']
+}
+
+
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+module.exports = { sum, fromEuroToDollar }
